fd91baebe58d13e5782060c88f79e655a174bfc9
Umme Mohona <ummaymohona488@gmail.com>
2023-07-11 19:33:11 +0600

    content part fixed

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,41 +1,43 @@
 
 
 const Content = () => {
-    const  handleButtonClick = () => {
+    const handleButtonClick = () => {
         const textArea = document.getElementById('displayText');
-        textArea.value = "Every traveler carries a unique tale, woven with the threads of unforgettable experiences and cherished memories. From the hidden gems stumbled upon to the serendipitous encounters with locals, each journey leaves an indelible mark on the soul. The laughter shared, the challenges conquered, and the moments of awe are all part of the traveler's tapestry. Every destination becomes a chapter, and every story told becomes an inspiration for those who long to explore. So, come forth, fellow wanderers, and let us share our travel stories, weaving a tapestry of wanderlust that connects us all";
-      };
+        const displayArea = document.getElementById('displayArea');
+        displayArea.innerHTML = textArea.value;
+    };
 
-      const containerStyle = {
+    const containerStyle = {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
         padding: '20px',
-      };
-      
-      const buttonStyle = {
+    };
+
+    const buttonStyle = {
         margin: '10px',
         backgroundColor: '#f3d8a0',
         color: '#fff',
         border: '1px solid #f3d8a0',
         borderRadius: '8px',
         padding: '3px',
-      };
-      
-      const textAreaStyle = {
+    };
+
+    const textAreaStyle = {
         margin: '10px',
-        width: '550px',
-        height: '170px',
         color: '#ae7954',
         border: '1px solid #DDBB9C'
-      };
+    };
 
     return (
         <div style={containerStyle}>
-            <button style={buttonStyle} onClick={handleButtonClick}>Click here to see the Content</button>
+            <textarea style={textAreaStyle} id="displayText" />
+            <button style={buttonStyle} onClick={handleButtonClick}>
+                Click here
+            </button>
             <br />
-        <textarea style={textAreaStyle} id="displayText" defaultValue="See more..." readOnly />
+            <div id="displayArea"></div>
         </div>
     );
 };
